eeea94e54f655c7a5b37255dcd8bf9831f8059ca
Kautuk <kautukpandey2004@gmail.com>
2025-08-19 08:51:18 +0530

    19

--- src/assignment/Assignment.java
+++ src/assignment/Assignment.java
@@ -6,9 +6,28 @@ class MyClass {
         System.out.println(count);
     }
 }
+class Student {
+    String name;
+    int age;
+
+    Student(String n, int a) {
+        name = n;
+        age = a;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
 public class Assignment {
 	public static void main(String[]args) {
-		System.out.println(MyClass.count);
-        MyClass.display();
+//		System.out.println(MyClass.count);
+//        MyClass.display();
+		
+		Student s1 = new Student("Alice", 20);
+        Student s2 = new Student("Bob", 22);
+
+        s1.display();
+        s2.display();
 	}
 }
